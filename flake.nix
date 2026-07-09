134d2cd1af7da542f31a098404b5dfdf21f017df
Justin Tung <justin.tung@live.ca>
2026-07-08 22:59:13 -0400

    feat(nix): update flake to nixos template
    https://github.com/NixOS/templates/tree/master

--- flake.nix
+++ flake.nix
@@ -2,35 +2,47 @@
   description = "Rust language development flake";
   # Template from Vimjoyer-Effortless Rust Development with Nix Devshells, Packaging & Beyond
   # https://www.youtube.com/watch?v=Ss1IXtYnpsg
+  # and NixOS templates
+  # https://github.com/NixOS/templates/blob/master/rust/flake.nix
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
-    { self, nixpkgs }:
-    let
-      pkgs = nixpkgs.legacyPackages."x86_64-linux";
-    in
     {
-      devShells."x86_64-linux".default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          # Rust compiler, package and task management
-          rustc
-          cargo
-          # Rust Linter
-          clippy
-          # Rust LSP
-          rust-analyzer
-          # Rust formatter
-          rustfmt
-          # Other dependencies
-          # glib
-        ];
-        # Expose dependencies declared above
-        nativeBuildInputs = [ pkgs.pkg-config ];
-        # Tools need path
-        env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-      };
-    };
+      self,
+      nixpkgs,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              # Rust compiler, package and task management
+              rustc
+              cargo
+              # Rust Linter
+              clippy
+              # Rust LSP
+              rust-analyzer
+              # Rust formatter
+              rustfmt
+              # Other dependencies
+              # glib
+            ];
+            # Tools need path
+            RUST_SRC_PATH = rustPlatform.rustLibSrc;
+            # Expose dependencies declared above
+            nativeBuildInputs = [ pkgs.pkg-config ];
+          };
+      }
+    );
 }
